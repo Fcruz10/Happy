17ef2361f9197a3875ed65f4248da63680f0b380
Francisco Cruz <francisco.joao.cruz10@gmail.com>
2020-10-15 12:03:12 +0100

    update: add orphanage.html&.css&.js

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,29 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+//create map
+const map = L.map('mapid', options).setView([38.7206143,-9.1459582], 15);
+
+//create and add titleLayer
+L
+.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+.addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+
+//create and add marker
+L
+.marker([38.7206143,-9.1459582], { icon })
+.addTo(map)
