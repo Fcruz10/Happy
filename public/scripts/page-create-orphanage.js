e1686c7cb10a3286bfe1dc6d73ec091d3a591388
Francisco Cruz <francisco.joao.cruz10@gmail.com>
2020-10-17 02:36:11 +0100

    update: add required fields and new field functionality .html & .js

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -11,19 +11,63 @@ const icon = L.icon({
     iconUrl: "./public/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
 });
 
-//create popup overlay
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent(`Girls' Home <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>`);
+let marker;
 
 //create and add marker
-L
-.marker([38.7206143,-9.1459582], { icon })
-.addTo(map)
-.bindPopup(popup);
+map.on('click', (event) => {
+    const lat = event.latlng.lat;
+    const lng = event.latlng.lng;
+
+    document.querySelector('[name=lat]').value = lat;
+    document.querySelector('[name=lng]').value = lng;
+
+    //remove icon
+    marker && map.removeLayer(marker);
+
+    //add icon layer
+    marker = L.marker([lat, lng], { icon })
+    .addTo(map);
+});
+
+//add photo field
+function addPhotoField() {
+    //grab photo container #images
+    const container = document.querySelector('#images');
+
+    //grab container to duplicate .new-upload
+    const fieldsContainer = document.querySelectorAll('.new-upload');
+
+    //make clone of last image added
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true);
+
+    //check if field is empty, if yes, don't add to container #images
+    const input = newFieldContainer.children[0];
+
+    if(input.value == '') {
+        return 
+    };
+
+    //clean field before add to container #images
+    input.value = '';
+
+    //add clone to container #images
+    container.appendChild(newFieldContainer);
+};
+
+//delete new field
+function deleteField(event) {
+    const span = event.currentTarget;
+    
+    const fieldsContainer = document.querySelectorAll('.new-upload');
+
+    if(fieldsContainer.length <= 1) {
+        //clean field value
+        span.parentNode.children[0].value = '';
+        return
+    };
+
+    //delete field
+    span.parentNode.remove();
+}
