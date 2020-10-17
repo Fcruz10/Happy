4276c09fc24529e67f68e29a84e76162448c9d8b
Francisco Cruz <francisco.joao.cruz10@gmail.com>
2020-10-17 15:35:22 +0100

    chore: add hbs template engine

--- src/server.js
+++ src/server.js
@@ -1,12 +1,24 @@
 //import dependecy
 const express = require('express');
+const path = require('path');
+
+
 //express init
 const server = express();
 
-//crete route
-server.get('/', () => {
-    console.log('eeeeee')
-});
+server
+    //import static files
+    .use(express.static('public'))
+
+    //configure template engine -> handlebars.js
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'hbs')
+
+    //crete route
+    .get('/', (req, res) => {
+    return res.render('index')
+    })
+
 
 //turn on server
-server.listen(5500)
+server.listen(5500);
