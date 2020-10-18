8fa8a98b5d6c9a53e0e23217bc242368d2f8d0dd
Francisco Cruz <francisco.joao.cruz10@gmail.com>
2020-10-18 22:10:57 +0100

    refactor: updates the pages to dynamic w/ backend data

--- src/pages.js
+++ src/pages.js
@@ -1,20 +1,50 @@
-const orphanages = require('./database/fakedata.js')
+const Database = require('./database/db');
+const saveOrphanage = require('./database/saveOrphanage');
+
 
 module.exports = {
     index(req, res) {
-        return res.render('index')
+        return res.render('index');
     },
 
-    orphanage(req, res) {
-        return res.render('orphanage')
+    async orphanage(req, res) {
+
+        const id = req.query.id;
+
+        try {
+            const db = await Database;
+            const results = await db.all(`SELECT * FROM orphanages WHERE id = "${id}"`);
+            const orphanage = results[0];
+            orphanage.images = orphanage.images.split(",");
+            
+            orphanage.firstImage = orphanage.images[0];
+            if(orphanage.open_on_weekends == '0') {
+                orphanage.open_on_weekends = false;
+            } else {
+                orphanage.open_on_weekends = true;
+            }
+
+            return res.render('orphanage', { orphanage });
+        } catch (error) {
+            return res.send('Database Error!');
+        }
+
     },
 
-    orphanages(req, res) {
-        return res.render('orphanages', { orphanages })
+    async orphanages(req, res) {
+        try {
+            const db = await Database;
+            const orphanages = await db.all("SELECT * FROM orphanages");
+
+            return res.render('orphanages', { orphanages });
+    
+        } catch (error) {
+            return res.send('Database Error!');
+        }
     },
 
     createOrphanage(req, res) {
-        return res.render('create-orphanage')
+        return res.render('create-orphanage');
 
     }
-}
+};
